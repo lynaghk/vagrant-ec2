e6e46aaebf5a33259bac7265f77a56f63e247aab
Kevin J. Lynagh <kevin@keminglabs.com>
2011-12-08 13:04:42 -0800

    Bootstrap script should ensure libopenssl-ruby is installed (required for Chef gem).

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,6 @@ log_level :debug" > $CHEF_FILE_CACHE_PATH/solo.rb
 
 
 apt-get update
-apt-get --no-install-recommends -y install build-essential ruby ruby-dev rubygems
+apt-get --no-install-recommends -y install build-essential ruby ruby-dev rubygems libopenssl-ruby
 gem install --no-rdoc --no-ri chef --version=0.9.12
 echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/gems/1.8/bin"' > /etc/environment
