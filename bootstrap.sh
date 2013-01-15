724738a9a3620bcec78b940442e50541e0f661f9
Kevin J. Lynagh <kevin@keminglabs.com>
2013-01-16 09:54:18 +1300

    Update Chef gem version to 10.16.6 because 0.10.10 now blows up with "moneta/basic_file" load error.
    Dear Ruby community: what's the point of versioning a package if you're going to let its dependencies go crazy?

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,5 +21,5 @@ log_level :debug" > $CHEF_FILE_CACHE_PATH/solo.rb
 
 apt-get update
 apt-get --no-install-recommends -y install build-essential ruby ruby-dev rubygems libopenssl-ruby
-gem install --no-rdoc --no-ri chef --version=0.10.10
+gem install --no-rdoc --no-ri chef --version=10.16.6
 echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/gems/1.8/bin"' > /etc/environment
